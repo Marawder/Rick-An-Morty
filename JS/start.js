eef14b9c9473af231b7fc83549a655b7eaca9647
Krestina <43370282+DinaDimsen@users.noreply.github.com>
2019-03-26 11:15:01 +0100

    spalsh
    Start side med splash og javascript

---
+++ JS/start.js
@@ -0,0 +1,46 @@
+function fade(element) {
+        var op = 1;  // initial opacity
+        var timer = setInterval(function () {
+            if (op <= 0.1){
+                clearInterval(timer);
+
+                element.style.display = 'none';
+            }
+            element.style.opacity = op;
+            element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+            op -= op * 0.1;
+        }, 50);
+    }
+
+        setTimeout(function(){ 
+
+                 if(typeof(Storage) !== "undefined") {
+
+                  console.log("Already shown" +sessionStorage.getItem('spalashShown'));
+
+                   if( !sessionStorage.getItem('spalashShown') || sessionStorage.getItem('spalashShown') === null ) {  
+
+                   document.getElementById('splash') .style.display = 'inline';
+
+                    //Display splash
+                    setTimeout(function(){   
+
+                     fade(document.getElementById('splash'));
+                      
+
+                     sessionStorage.setItem('spalashShown', true  );
+                  }
+                   , 5000);
+
+                      } else {
+
+                         //Display Main Content
+                          document.getElementById('splash') .style.display = 'none'
+                            console.log("Already shown");
+                         }
+                      }
+
+                    else {
+                            document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+                          }
+                             }, 0);
